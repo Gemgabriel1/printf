647035611879c9fb9faf793dfed7ca71b39166ec
Gemgabriel1 <101466218+Gemgabriel1@users.noreply.github.com>
2022-05-23 11:34:59 +0100

    printing

--- _printf.c
+++ _printf.c
@@ -1,53 +1,82 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-  * _printf - function that prints to std
-  * @format: character string
-  * Return: the number of characters printed
-  * excluding the null byte used to end output to strings
-  */
-
-int _printf(const char *format, ...)
+ *check_for_specifiers - checks if there is a valid format specifier
+ *@format: possible format specifier
+ *
+ *Return: pointer to valid function or NULL
+ */
+static int (*check_for_specifiers(const char *format))(va_list)
 {
-	int count = 0;
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+	};
 
-	va_list args;
-	int i = 0, (*func_select)(va_list);
-
-	va_start(args, format);
-
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-
-	while (format[i] != '\0')
+	for (i = 0; p[i].t != NULL; i++)
 	{
-		if (format[i] != '%')
+		if (*(p[i].t) == *format)
 		{
-			count += _putchar(format[i]);
+			break;
 		}
-		else
-		{
-			if (format[i + 1] == '%')
-			{
-				count += _putchar('%');
-				i++;
-			}
-			else
-			{
-				func_select = selector(format[i + 1]);
-				if (func_select != NULL)
-				{
-					count += func_select(args);
-					i++;
-				}
-			}
-		}
-		i++;
 	}
-	va_end(args);
-	return (count);
+	return (p[i].f);
 }
 
+/**
+ *_printf - prints anything
+ *@format: list of argument types to be passed to the function
+ *
+ *Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(valist);
+	return (count);
+}
