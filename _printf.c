34478e04d92db5aac54e29634d5ea5a4598a3ebe
Israel025 <Israeltademola@gmail.com>
2022-05-23 09:55:26 +0000

    correcting errors in the codes

--- _printf.c
+++ _printf.c
@@ -16,8 +16,12 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (format[0] == '%' && format[1] == '\0')
-		return (count);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+
 	while (format[i] != '\0')
 	{
 		if (format[i] != '%')
