dd366fb30e9427e0a35f58c242ae62f3ae341f93
Israel025 <Israeltademola@gmail.com>
2022-05-24 17:15:49 +0000

    more modifications

--- functions3.c
+++ functions3.c
@@ -110,11 +110,11 @@ int print_percent(va_list l, flags_t *f)
 }
 
 /**
- * print_int - prints an integer
- * @l: va_list of arguments from _printf
+ * print_int - prints an integer.
+ * @l: va_list of arguments from _printf.
  * @f: pointer to the struct flags determining
- * if a flag is passed to _printf
- * Return: number of char printed
+ * if a flag is passed to _printf.
+ * Return: The number of char printed
  */
 int print_int(va_list l, flags_t *f)
 {
@@ -128,5 +128,6 @@ int print_int(va_list l, flags_t *f)
 	if (n <= 0)
 		res++;
 	print_number(n);
+
 	return (res);
 }
