96f9ccf89e9ceff90938c0cf43750a80c117450e
Israel025 <Israeltademola@gmail.com>
2022-05-24 17:02:46 +0000

    more modifications

--- functions1.c
+++ functions1.c
@@ -31,7 +31,7 @@ char *convert(unsigned long int num, int base, int lowercase)
  * get_flag - this turns on flags if _printf finds
  * a flag modifier in the format string
  * @s: character that holds the flag specifier
- * @f: pointer to the struct flags in which we turn the flags on
+ * @f: pointer to the struct flags that turns the flags on
  *
  * Return: 1 if a flag has been turned on, 0 otherwise.
  */
