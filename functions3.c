16a6ae5737665cc193b2d0c9f3cfaf52a02fa2e9
Israel025 <Israeltademola@gmail.com>
2022-05-24 17:14:50 +0000

    more modifications

--- functions3.c
+++ functions3.c
@@ -96,15 +96,16 @@ int print_rot13(va_list l, flags_t *f)
 }
 
 /**
- * print_percent - prints a percent
- * @l: va_list arguments from _printf
- * @f: pointer to the struct flags in which we turn the flags on
- * Return: number of char printed
+ * print_percent - prints a %(percent) character
+ * @l: va_list arguments from _printf.
+ * @f: pointer to the struct flags in which we turn the flags on.
+ * Return: The number of char printed
  */
 int print_percent(va_list l, flags_t *f)
 {
 	(void)l;
 	(void)f;
+
 	return (_putchar('%'));
 }
 
