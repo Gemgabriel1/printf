08e6bbbaf1b22d1a74877df7995670c3b37ebbc1
Israel025 <Israeltademola@gmail.com>
2022-05-24 17:10:50 +0000

    more modifications

--- functions2.c
+++ functions2.c
@@ -88,7 +88,8 @@ int print_binary(va_list l, flags_t *f)
  * print_octal - prints a number in base 8
  * @l: va_list arguments from _printf
  * @f: pointer to the struct that determines
- * if a flag is passed to _printf
+ * if a flag is passed to _printf.
+ *
  * Description: the function calls convert() which in turns converts the input
  * number into the correct base and returns it as a string
  * Return: the number of char printed
@@ -101,6 +102,8 @@ int print_octal(va_list l, flags_t *f)
 
 	if (f->hash == 1 && str[0] != '0')
 		count += _putchar('0');
+
 	count += _puts(str);
+
 	return (count);
 }
