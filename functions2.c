be07e80283dc15c54ed4f74558eb40df1441fe32
Israel025 <Israeltademola@gmail.com>
2022-05-24 17:10:04 +0000

    more modifications

--- functions2.c
+++ functions2.c
@@ -60,6 +60,7 @@ int print_hex_big(va_list l, flags_t *f)
 	if (f->hash == 1 && str[0] != '0')
 		count += _puts("0X");
 	count += _puts(str);
+
 	return (count);
 }
 
@@ -67,7 +68,8 @@ int print_hex_big(va_list l, flags_t *f)
  * print_binary - prints a number in base 2
  * @l: va_list arguments from _printf
  * @f: pointer to the struct that determines
- * if a flag is passed to _printf
+ * if a flag is passed to _printf.
+ *
  * Description: the function calls convert() which in turns converts the input
  * number into the correct base and returns it as a string
  * Return: the number of char printed
@@ -78,6 +80,7 @@ int print_binary(va_list l, flags_t *f)
 	char *str = convert(num, 2, 0);
 
 	(void)f;
+
 	return (_puts(str));
 }
 
