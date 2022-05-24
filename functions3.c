bb466604ff40b7003f63b511fff1df0cb2c96f4c
Israel025 <Israeltademola@gmail.com>
2022-05-24 17:13:41 +0000

    more modifications

--- functions3.c
+++ functions3.c
@@ -66,10 +66,10 @@ int print_rev(va_list l, flags_t *f)
 
 /**
  * print_rot13 - prints a string using rot13
- * @l: list of arguments from _printf
+ * @l: list of arguments from _printf.
  * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
- * Return: length of the printed string
+ * if a flag is passed to _printf.
+ * Return: The length of the printed string
  */
 int print_rot13(va_list l, flags_t *f)
 {
@@ -92,7 +92,6 @@ int print_rot13(va_list l, flags_t *f)
 			}
 		}
 	}
-
 	return (j);
 }
 
