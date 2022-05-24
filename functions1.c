5625d9e774b86ef8b285ab2a78308dfcaf5daa82
Israel025 <Israeltademola@gmail.com>
2022-05-24 17:01:13 +0000

    more modifications

--- functions1.c
+++ functions1.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * convert - converts number and base into string
+ * convert - converts number and base into string.
  * @num: input number
  * @base: input base
  * @lowercase: flag if hexa values need to be lowercase
