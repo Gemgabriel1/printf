258b8b7dd4a8db643293f0f8fed908bf4307c10d
Israel025 <Israeltademola@gmail.com>
2022-05-24 17:07:06 +0000

    more modifications

--- functions1.c
+++ functions1.c
@@ -124,11 +124,11 @@ int print_address(va_list l, flags_t *f)
 
 /**
  * print_string - loops through a string and prints
- * every character
- * @l: va_list arguments from _printf
+ * every character.
+ * @l: va_list arguments from _printf.
  * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
- * Return: number of char printed
+ * if a flag is passed to _printf.
+ * Return: number of char printed.
  */
 int print_string(va_list l, flags_t *f)
 {
@@ -138,5 +138,6 @@ int print_string(va_list l, flags_t *f)
 
 	if (!s)
 		s = "(null)";
+
 	return (_puts(s));
 }
